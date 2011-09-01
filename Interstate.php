09f0bf6a7fc2e529ecec368dda913f4a0a8ce5f1
Simon Fletcher <simon@bakedcode.com>
2011-09-01 12:30:55 -0700

    Initial commit

---
+++ Interstate.php
@@ -0,0 +1,277 @@
+<?php
+
+class Interstate {
+
+	const INTERSTATE_HOST	= 'interstateapp.com';
+	const API_HOST			= 'api.interstateapp.com';
+	const API_VERSION		= 2;
+	private $_clientId;
+	private $_secret;
+	private $_redirectUri;
+	private $_oauthToken;
+	private $_https		= true;
+
+	public function __construct( $params ) {
+	
+		// { 'client_id', 'secret', 'redirect_uri', 'oauth_token', 'https' }
+	
+		if( isset( $params[ 'client_id' ] ) ) {
+			
+			$this->_clientId = $params[ 'client_id' ];
+		
+		}
+	
+		if( isset( $params[ 'client_secret' ] ) ) {
+			
+			$this->_secret = $params[ 'client_secret' ];
+		
+		}
+		
+		if( isset( $params[ 'redirect_uri' ] ) ) {
+			
+			$this->_redirectUri = $params[ 'redirect_uri' ];
+		
+		}
+	
+		if( isset( $params[ 'oauth_token' ] ) ) {
+			
+			$this->_oauthToken = $params[ 'oauth_token' ];
+		
+		}
+	
+		if( isset( $params[ 'https' ] ) ) {
+			
+			$this->_https = (bool)$params[ 'htpss' ];
+		
+		}
+		
+	}
+	
+	public function getApiUrl() {
+		
+		return ( ( $this->_https ) ? 'https://' : 'http://' ) . self::API_HOST . '/v' . self::API_VERSION . '/';
+		
+	
+	}
+	
+	public function getRootUrl() {
+		
+		return ( ( $this->_https ) ? 'https://' : 'http://' ) . self::INTERSTATE_HOST . '/';
+		
+	
+	}
+	
+	public function getAuthorizeUrl() {
+
+		return $this->getRootUrl() . 'oauth2/authorize?client_id=' . $this->_clientId . '&redirect_uri=' . $this->_redirectUri . '&response_type=code';
+
+	}
+	
+	public function sign( $uri ) {
+	
+		if( $this->_oauthToken === null ) {
+			
+			return $uri;
+		
+		} else {
+			
+			$parts = parse_url( $uri );
+			
+			if( isset( $parts[ 'query' ] ) !== false ) {
+			
+				$uri .= '&oauth_token=' . $this->_oauthToken;
+			
+			} else {
+				
+				$uri .= '?oauth_token=' . $this->_oauthToken;
+			
+			}
+			
+			return $uri;
+		
+		}
+			
+	}
+	
+	public function setAccessToken( $token ) {
+	
+		$this->_oauthToken = $token;
+		
+		return $this;
+		
+	}
+	
+	public function getAccessToken( $code, $type = 'authorization_code', $setToken = true ) {
+	
+		$post	= array(
+			
+			'client_id'		=> $this->_clientId,
+			'client_secret'	=> $this->_secret,
+			'redirect_uri'	=> $this->_redirectUri
+		
+		);
+		
+		switch( $type ) {
+			
+			default:
+				case 'authorization_code':
+				
+				$post += array(
+					
+					'grant_type'	=> 'authorization_code',
+					'code'			=> $code
+				
+				);
+				
+				break;
+				
+			case 'refresh_token':
+				
+				$post += array(
+				
+					'grant_type'	=> 'refresh_token',
+					'refresh_token'	=> $code
+				
+				);
+				
+				break;
+		
+		}
+		
+		try {
+			
+			$response = $this->fetch( array(
+				
+				'url'		=> 'oauth2/token',
+				'post'		=> $post
+			
+			));
+			
+			if( $setToken ) {
+			
+				$this->setAccessToken( $response[ 'access_token' ] );
+			
+			}
+			
+			return $response;
+			
+		} catch( Exception $e ) {
+			
+			return false;
+		
+		}
+		
+	}
+
+	public function fetch( $params, $post = array() ) {
+		
+		if( is_string( $params ) ) {
+			
+			$params = array( 'url' => $params );
+		
+		}
+		
+		if( isset( $params[ 'post' ] ) === false ) {
+			
+			$params[ 'post' ] = $post;
+		
+		}
+		
+		if( preg_match( '/\s/', $params[ 'url' ] ) === 1 ) {
+			
+			$explode			= explode( ' ', $params[ 'url' ] );
+			$params[ 'verb' ]	= strtoupper( $explode[ 0 ] );
+			$params[ 'url' ]	= $explode[ 1 ];
+			
+		}
+		
+		if( $params[ 'url' ][ 0 ] == '/' ) {
+			
+			$params[ 'url' ] = substr( $params[ 'url' ], 1, strlen( $params[ 'url' ] ) );
+		
+		}
+		
+		$url	= $this->sign( $this->getApiUrl() . $params[ 'url' ] );
+		$curl	= curl_init();
+		
+		curl_setopt( $curl, CURLOPT_URL, $url );
+		curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+		
+		if( isset( $params[ 'post' ] ) !== false ) {
+		
+			curl_setopt( $curl, CURLOPT_POST, true );
+			curl_setopt( $curl, CURLOPT_POSTFIELDS, $params[ 'post' ] );
+		
+		}
+		
+		if( isset( $params[ 'verb' ] ) !== false ) {
+		
+			curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, $params[ 'verb' ] );
+		
+		}
+		
+		if( isset( $params[ 'onCurl' ] ) !== false ) {
+		
+			$params[ 'onCurl' ]( $this, $curl );
+		
+		}
+		
+		if( isset( $params[ 'curlopt' ] ) !== false ) {
+			
+			curl_setopt_array( $curl, $params[ 'curlopt' ] );
+		
+		}
+		
+		$response	= curl_exec( $curl );
+		$code		= curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+
+		var_dump($uri,$params['post'],$response);die;
+
+		if( $code === 200 ) {
+			
+			$response = $this->_parseResponse( $response );
+			
+			if( isset( $response[ 'status' ] ) !== false ) {
+			
+				if( $response[ 'status' ] != 'error' ) {
+					
+					return ( isset( $response[ 'response' ] ) ? $response[ 'response' ] : true );
+				
+				} else {
+					
+					throw new Exception();
+				
+				}
+			
+			} else {
+			
+				return $response;
+			
+			}
+			
+		} else {
+		
+			if( $response != '' ) {
+				
+				$response = $this->_parseResponse( $response );
+				
+				throw new Exception( ( is_array( $response[ 'error' ] ) ? $response[ 'error' ][ 'message' ] : $response[ 'error' ] ), $info[ 'http_code' ] );
+			
+			} else {
+				
+				throw new Exception( '', $info[ 'http_code' ] );
+			
+			}
+		
+		}
+	
+	}
+
+	protected function _parseResponse( $response ) {
+		
+		return json_decode( $response, true );
+	
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,74 @@
+Interstate PHP SDK
+==================
+
+This is the new base SDK for working with the [Interstate API v2](http://developers.interstateapp.com/v2).
+
+## Getting Started
+To get started with the new sdk, include Interstate.php in your app and initialize an instance of the Interstate class, with your OAuth app's settings.
+
+```php
+$instance = new Interstate( array(
+
+	'client_id'		=> 'CLIENT_ID',
+	'client_secret'	=> 'CLIENT_SECRET',
+	'redirect_uri'	=> 'REDIRECT_URI'
+	//'oauth_token	=> 'OAUTH_TOKEN',
+	//'https'		=> false
+
+));
+```
+Now you can call any method on the `$instance` object. The `$instance` object has the following methods:
+
+* `getApiUrl()`
+* `getRootUrl()`
+* `getAuthorizeUrl()`
+* `sign( $uri )`
+* `setAccessToken( $oauth_token )`
+* `getAccessToken( $code, $type = 'authorization_code', $setToken = true )`
+* `fetch( $params, $post = array() )`
+
+## Authentication
+
+To authenticate a user, first redirect them to the Interstate OAuth Authorization page. You can get this URL by calling the `getAuthorizeUrl()` method, for example:
+
+```php
+header( 'Location: ' . $instance->getAuthorizeUrl() );
+```
+
+If the user approves your authorization request, they will be sent back to your app, with a query string parameter with the key `code`. Grab this code and then run the `getAccessToken()` method to convert the code in to an `oauth_token` and a `refresh_token`.
+
+```php
+$tokens = $instance->getAccessToken( $_GET[ 'code' ] ); // { oauth_token: '..', refresh_token: '..' }
+``
+
+## Refresh Tokens
+
+Each oauth_token only lasts for a max of 3600 seconds (1 hour), so when your `oauth_token` expires, you will need to generate a new one, by using the refresh_token you received when you ran `getAccessToken()`. To use your `refresh_token` to generate a new `oauth_token`, use the `getAccessToken()` method again, like so:
+
+```php
+$newTokens = $instance->getAccessToken( $refresh_token, 'refresh_token' );
+```
+
+When you call this method, you will be returned with both a new `oauth_token` and a new `refresh_token`. When the new `oauth_token` expires in 3600 seconds, you simply repeat the proces.
+
+## Making requests
+
+Once you've authenticated with a user or manually set an `oauth_token` (either in the Interstate `__construct` or by calling `setAccessToken()`), you are able to make authenticated requests to the Interstate API. You can make custom requests by using the `fetch()` method, like so:
+
+```php
+$response = $instance->fetch( '/roadmap/4e2e8e88e962317e16000000' );
+```
+
+If you're not trying to make a `GET` request (e.g. a `DELETE` request), simply either specify in the URL parameter or pass an array instead.
+
+```php
+$response = $instance->fetch( 'DELETE /roadmap/4e2e8e88e962317e16000000' );
+$response = $instance->fetch( array( 'url' => 'roadmap/4e2e8e88e962317e16000000', 'verb' => 'DELETE' ) )
+```
+
+If you need to post data, you can pass an array of key => values as second argument to the method, or pass a single array as the only argument with a `post` key.
+
+```php
+$response = $instance->fetch( 'PUT /roadmap/4e2e8e88e962317e16000000', array( 'title' => 'New title' ) );
+$response = $instance->fetch( array( 'url' => '/roadmap/4e2e8e88e962317e16000000', 'verb' => 'PUT', 'post' => array( 'title' => 'New title' ) );
+```
